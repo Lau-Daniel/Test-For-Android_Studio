37f42eb9152eb5f04ef86e7eed04c0d420f5664f
Lau-Daniel <2067168016@qq.com>
2017-03-09 22:53:11 +0800

    第一次提交

--- app/src/main/java/com/example/lzl/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lzl/myapplication/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.lzl.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.lzl.mylibrary.ASdsd;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ASdsd.run();
+        Toast.makeText(this,"嘿嘿",0).show();
+
     }
 
 
---
+++ mylibrary/src/main/java/com/lzl/mylibrary/ASdsd.java
@@ -0,0 +1,11 @@
+package com.lzl.mylibrary;
+
+/**
+ * Created by lzl on 2017/3/9.
+ */
+
+public class ASdsd {
+    public static void run(){
+        System.out.print("嘿嘿");
+    }
+}
